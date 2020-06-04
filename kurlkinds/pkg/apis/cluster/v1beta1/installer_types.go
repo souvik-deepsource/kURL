7883f27eaec8cb542247b875546580498db58fdd
Michael <michael@replicated.com>
2020-06-04 21:40:56 +0000

    ADD containerd kurlkind

--- kurlkinds/pkg/apis/cluster/v1beta1/installer_types.go
+++ kurlkinds/pkg/apis/cluster/v1beta1/installer_types.go
@@ -39,6 +39,7 @@ type InstallerSpec struct {
 	IptablesConfig  IptablesConfig  `json:"iptablesConfig,omitempty"`
 	FirewalldConfig FirewalldConfig `json:"firewalldConfig,omitempty"`
 	Ekco            Ekco            `json:"ekco,omitempty"`
+	Containerd      Containerd      `json:"containerd,omitempty"`
 }
 
 type Contour struct {
@@ -173,6 +174,10 @@ type Ekco struct {
 }
 
 type Calico struct {
+	Version string `json:"version"`
+}
+
+type Containerd struct {
 	Version string `json:"version"`
 }
 
